35927e907aaad25a4a5557a3494ab3cffc99b86e
Alvin <fulmar38251@letterboxes.org>
2020-08-06 19:34:42 -0400

    Update logosRoutes for post route

--- src/routes/logosRoutes.js
+++ src/routes/logosRoutes.js
@@ -1,9 +1,21 @@
 
 const router = require('express').Router();
 const {deleteLogo, updateLogo, createLogo, getLogos} = require('../database/logos');
+const { route } = require('./productsRoutes');
 
 router.get('/', async (req, res) => {
   res.send(await getLogos());
 });
+// add post for logos
+router.post('/', async (apiReq, apiResp) => {
+  const newLogo = apiReq.body;
+  await createLogo(newLogo);
+  apiResp.send({
+    message: 'New logo created.',
+    allLogos: await getLogos(),
+    thanks: true
+  })
+})
+
 
 module.exports = router;
